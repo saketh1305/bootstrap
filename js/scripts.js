70a1f99f6ba909ff395385c1d476d1a9ced740c0
Saketh Reddy <sakethreddy1305@gmail.com>
2020-05-22 18:35:03 +0530

    End of BootStrap

--- js/scripts.js
+++ js/scripts.js
@@ -12,4 +12,10 @@ $(document).ready(function(){
             $('#carouselButton').children('span').addClass('fa-pause');
         }
     });
+    $('#reserveButton').click(function() {
+        $('#reserveModal').modal('toggle');
+    });
+    $('#loginButton').click(function() {
+        $('#loginModal').modal('toggle');
+    });
 });
